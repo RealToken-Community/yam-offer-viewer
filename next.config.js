631dd816cb34ffd064210dfd779cbe8de84efccc
Sigri44 <sigri44@hotmail.fr>
2026-01-17 23:21:22 +0100

    Clean next config

--- next.config.js
+++ next.config.js
@@ -2,20 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  env: {
-    // NEXT_PUBLIC_COMMUNITY_API_URI: process.env.NEXT_PUBLIC_COMMUNITY_API_URI,
-    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'realt.co',
-      },
-      // {
-      //   protocol: 'https',
-      //   hostname: 'api.realtoken.community',
-      // }
+      }
     ]
   },
 }
